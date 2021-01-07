bec698c678c444a5b3e4b13e70567c5184e53229
ugurcanozalp <uurcann94@gmail.com>
2021-01-07 18:51:17 +0300

    convert td3 to benchmark settings.

--- td3_agent.py
+++ td3_agent.py
@@ -44,8 +44,8 @@ class TD3Agent():
         self.critic_2_optim = optim.AdamW(self.train_critic_2.parameters(), lr=lr, weight_decay=weight_decay)
         print(f'Number of paramters of Single Critic Net: {sum(p.numel() for p in self.train_critic_2.parameters())}')
 
-        self.noise_generator = OrnsteinUhlenbeckNoise(mu=np.zeros(action_size), theta=1.2, sigma=0.55, dt=0.02)
-        self.target_noise = GaussianNoise(mu=np.zeros(action_size), sigma=0.1, clip=0.5)
+        self.noise_generator = OrnsteinUhlenbeckNoise(mu=np.zeros(action_size), theta=1.2, sigma=0.2, dt=0.02)
+        self.target_noise = GaussianNoise(mu=np.zeros(action_size), sigma=0.2, clip=0.5)
         
         self.memory= ReplayBuffer(action_size= action_size, buffer_size= buffer_size, \
             batch_size= self.batch_size, device=self.device)
