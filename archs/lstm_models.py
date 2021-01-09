391a929c462e5e5418625198f4207b4c8f01b431
ugurcanozalp <uurcann94@gmail.com>
2021-01-09 13:59:23 +0300

    arch changed.

--- archs/lstm_models.py
+++ archs/lstm_models.py
@@ -52,16 +52,17 @@ class Critic(nn.Module):
         self.action_dim = action_dim
 
         self.state_encoder = NormalizedLSTM(input_size=self.state_dim, hidden_size=96, batch_first=True, bidirectional=False, num_layers=1)
-        self.action_encoder = nn.Sequential(nn.Linear(self.action_dim, 96), nn.GELU())
-
+        self.action_encoder = nn.Sequential(nn.Linear(self.action_dim, 96), nn.Tanh())
+        nn.init.xavier_uniform_(self.action_encoder[0], gain=nn.init.calculate_gain('tanh'))
+        
         self.fc2 = nn.Linear(96,256)
-        nn.init.xavier_uniform_(self.fc2.weight, gain=0.1*nn.init.calculate_gain('tanh'))
+        nn.init.xavier_uniform_(self.fc2.weight, gain=0.1*nn.init.calculate_gain('relu'))
         
         self.fc_out = nn.Linear(256,1)
         nn.init.xavier_uniform_(self.fc_out.weight)
         nn.init.zeros_(self.fc_out.bias)
 
-        self.act = nn.Tanh()
+        self.act = nn.GELU()
 
     def forward(self, state, action):
         """
--- archs/transformer_models.py
+++ archs/transformer_models.py
@@ -60,16 +60,17 @@ class Critic(nn.Module):
         
         self.state_encoder = StableTransformerEncoder(num_layers=1, d_in=self.state_dim, 
             d_model=96, nhead=2, dim_feedforward=192, dropout=0.0, use_gate = False)
-        self.action_encoder = nn.Sequential(nn.Linear(self.action_dim, 96), nn.GELU())
+        self.action_encoder = nn.Sequential(nn.Linear(self.action_dim, 96), nn.Tanh())
+        nn.init.xavier_uniform_(self.action_encoder[0], gain=nn.init.calculate_gain('tanh'))
 
         self.fc2 = nn.Linear(96,256)
-        nn.init.xavier_uniform_(self.fc2.weight, gain=nn.init.calculate_gain('tanh'))
+        nn.init.xavier_uniform_(self.fc2.weight, gain=nn.init.calculate_gain('relu'))
         
         self.fc_out = nn.Linear(256,1)
         nn.init.xavier_uniform_(self.fc_out.weight)
         nn.init.zeros_(self.fc_out.bias)
 
-        self.act = nn.Tanh()
+        self.act = nn.GELU()
 
     def forward(self, state, action):
         """
