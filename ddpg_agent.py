b1d6990dd7ec16daa38c274ac99d4f74805cb9de
ugurcanozalp <uurcann94@gmail.com>
2021-07-20 16:46:11 +0300

    prelu figure and lr

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -10,7 +10,7 @@ from get_optimizer import get_optimizer
 class DDPGAgent():
     rl_type = 'ddpg'
     def __init__(self, Actor, Critic, state_size=24, action_size=4, 
-            lr=2e-4, weight_decay=0, gamma=0.98, tau=0.001, batch_size=64, buffer_size=int(1e6)):
+            lr=5e-4, weight_decay=0, gamma=0.98, tau=0.001, batch_size=64, buffer_size=int(1e6)):
         
         self.state_size = state_size
         self.action_size = action_size
